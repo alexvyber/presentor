85469e04d2d420ea24b1ac4d7a23a1dc9e72c353
Alexey Sokolov <alexvyber@gmail.com>
2023-04-22 21:21:49 +0300

    remove slides

--- slides/components/presentation/Slide.tsx
+++
@@ -1,9 +0,0 @@
-import { cx } from 'cvax'
-
-export default function Slide({ children, id, className }) {
-  return (
-    <div id={id} className={cx('overflow-hidden ', className)}>
-      {children}
-    </div>
-  )
-}
